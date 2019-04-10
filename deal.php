c07e9147d3fe07b741e72b470f8466a28a642c10
harshitbansal373 <harshitbansal373@gmail.com>
2019-04-10 13:42:16 +0530

    deal.php added

---
+++ deal.php
@@ -0,0 +1,59 @@
+<?php include "includes/header.php"; ?>
+
+<?php include "includes/navigation.php"; ?>
+
+    <!-- Page Content -->
+    <div id="container">
+
+      <div class="row">
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-3">
+          <h3 class="ml-2">Filter</h3>
+          <img class="img-fluid" src="images/filterimage.png" alt="...">
+        </div>
+        
+        <!-- Blog Entries Column -->
+        <div class="col-md-6">
+            <h3 class="mb-4">Showing Result for "Search"</h3>
+
+          <!-- <div>
+            <img class="mr-5" style="float:left" src="http://placehold.it/300x300" alt="">
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+          </div> -->
+
+        <div class="media">
+          <img src="img2/mi-redmi-e7t-na-original-imafazxgdvdcnygz.jpeg" width="100" class="img-fluid webview align-self-center mr-3" alt="...">
+          <img src="img2/mi-redmi-e7t-na-original-imafazxgdvdcnygz.jpeg" width="80" class="img-fluid mobileview align-self-center mr-3" alt="...">
+          <!-- <img src="http://placehold.it/200x270" class="img-fluid webview align-self-center mr-3" alt="..."> -->
+          <!-- <img src="http://placehold.it/120x180" class="img-fluid mobileview align-self-center mr-3" alt="..."> -->
+          <div class="media-body">
+            <h5 class="mt-0"><b>Redmi Note 6 Pro(Rose Gold,64 GB)</b></h5>
+            <p>4 GB RAM | 64 GB ROM | Expandable Upto 256 GB <br>
+                15.9 cm (6.26 inch) FHD+ Display <br>
+                12MP + 5MP | 20MP + 2MP Dual Front Camera <br>
+                4000 mAh Li-polymer Battery <br>
+                Qualcomm Snapdragon 636 Processor
+            </p>
+            <button type="button" class="btn btn-primary">More Deals From</button>
+          </div>
+        </div>
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-3">
+            <img class="paidads" src="ads/pepsi-black-ad-bangalore-times-13-6-17.jpg" width="300" alt="">
+            <img class="paidads" src="ads/20110530125343_Capture.jpg" width="300" alt="">
+            <img class="paidads" src="ads/web.ae_.feministads.courtesy.picB_.jpg" width="300" height="200" alt="">
+        </div>
+
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+
+<?php include "includes/footer.php";?>
